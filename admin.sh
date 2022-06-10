7741d4ca2243f79a28ca42205cb37fabebf2bad5
Your Name <you@example.com>
2022-06-10 16:18:23 +0530

    admin.sh

---
+++ admin.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+echo "Hello starting script"
+cd ~/Desktop/AlgoTrader
+source venv/bin/activate
+cd AlgoTrader
+python manage.py makemigrations
+python manage.py migrate
+git add -u
+git commit -m "server changes"
+git pull
+git push
+python manage.py runserver
