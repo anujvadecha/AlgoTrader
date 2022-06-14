8b0f23601f86ca2d89f7c947f880f3738116e8a8
Vadecha <anuj.vadecha@ip-192-168-235-127.ap-south-1.compute.internal>
2022-06-14 10:36:25 +0530

    fixing date range for hourly candles

--- Core/Utils.py
+++ Core/Utils.py
@@ -23,9 +23,12 @@ def is_within_market_time_range(timestamp):
 def get_candle_time_series_for_date_range(from_date, to_date, interval: CandleInterval):
     import pandas as pd
     frequency_seconds = timeparse(interval.value) if interval != CandleInterval.day else 86400
-    from_date = ceil_dt(from_date, timedelta(seconds=frequency_seconds))
+
+    from_date = ceil_dt(from_date, timedelta(seconds=frequency_seconds)) if interval !=CandleInterval.hourly else ceil_dt(from_date, timedelta(seconds=timeparse(CandleInterval.fifteen_min.value))).replace(minute=15)
     LOGGER.info(f"date range requested from {from_date} to {to_date}")
+    print(f"date range requested from {from_date} to {to_date}")
     range = pd.date_range(start=from_date, end=to_date, freq=f"{frequency_seconds}S").to_pydatetime()
+    print(f"range from pandas is {range}")
     if interval == CandleInterval.day:
         return list(range)
     else:
