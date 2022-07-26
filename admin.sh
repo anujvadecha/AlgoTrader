b0979a40c292140d01805d76def4ad45790ff6ad
Your Name <you@example.com>
2022-07-26 15:48:01 +0530

    server changes

--- admin.sh
+++ admin.sh
@@ -3,10 +3,10 @@ echo "Hello starting script"
 cd ~/Desktop/AlgoTrader
 source venv/bin/activate
 cd AlgoTrader
-python manage.py makemigrations
-python manage.py migrate
 git add -u
 git commit -m "server changes"
 git pull
 git push
+python manage.py makemigrations
+python manage.py migrate
 python manage.py runserver
